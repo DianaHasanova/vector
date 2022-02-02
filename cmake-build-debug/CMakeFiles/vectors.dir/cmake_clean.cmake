889424313926c1c09ec87cbeda5f72d1728ddba2
DianaHasanova <dhasanova58@gmail.com>
2022-02-02 22:41:56 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/vectors.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vectors.dir/main.c.obj"
+  "libvectors.dll.a"
+  "vectors.exe"
+  "vectors.exe.manifest"
+  "vectors.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vectors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
